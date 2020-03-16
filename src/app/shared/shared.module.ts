e74bf303c6258a4edb5ec595b66231dcf1212937
pabloborrego <pablo.borrego@euigs.com>
2020-03-16 16:34:20 +0100

    new changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ForbiddenComponent } from './forbidden/forbidden.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-
 // Prime NG Components
 import { ButtonModule } from 'primeng/button';
 import { CodeHighlighterModule } from 'primeng/codehighlighter';
-import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -17,13 +18,17 @@ import { MenubarModule } from 'primeng/menubar';
     ButtonModule,
     TabViewModule,
     CodeHighlighterModule,
-    MenubarModule
+    MenubarModule,
+    InputTextModule,
+    FlexLayoutModule
   ],
   exports: [
     ButtonModule,
     TabViewModule,
     CodeHighlighterModule,
-    MenubarModule
+    MenubarModule,
+    InputTextModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
